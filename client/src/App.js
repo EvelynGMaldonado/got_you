aa3fc5fe937e438e53be07d37198133b4e64733c
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-11-22 17:36:45 -0800

    structure and models

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import UserProfile from './pages/UserProfile';
+import NotFound from './pages/NotFound';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp/>
+            </Route>
+            <Route exact path="/profile">
+              <UserProfile/>
+            </Route>
+            <Route>
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
+
