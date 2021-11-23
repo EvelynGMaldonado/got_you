5fadeeaafece7170ae47b7317a8c26d6b1574ed0
EvelynGMaldonado <evelyn.gmaldonado@gmail.com>
2021-11-23 14:42:02 -0800

    working on authorization

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,4 @@
 const User = require('./User');
 const ServicePost = require('./ServicePost');
 
-ServicePost.belongsTo(User, {
-    foreignKey: 'userId',
-    onDelete: 'CASCADE'
-});
 module.exports = { User, ServicePost };
