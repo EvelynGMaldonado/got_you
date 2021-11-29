ce53a4882d7eefd05ec780142f558088b93fc87b
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-11-28 18:50:48 -0800

    working on mutations, queries, resolvers, auth

---
+++ client/src/utils/auth.js
@@ -0,0 +1,46 @@
+//decode a token and het the user'sinfo out of it
+import decode from "jwt-decode";
+
+//create a class to instantiate for a user
+class Auth {
+    //get user data
+    getprofile() {
+        return decode(this.getToken());
+    }
+
+    //check if user is loggedIn
+    loggedIn() {
+        //check if there is a saved token and still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+        //handwaiving??
+    }
+    //check if token is expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
+    }
+    getToken() {
+        //get user token from local storage
+        return localStorage.getItem("id_token");
+    }
+    login(idToken) {
+        //we need to save the user's token to localstorage
+        localStorage.setItem("id_token", idToken);
+        window.location.assign("/");
+    }
+    logout() {
+        //clear user's token and profile data from localstorage
+        localStorage.removeItem("id_token");
+        //we'll reload the page and reset the state of the app
+        window.location.assign("/");
+    }
+}
+
+export default new Auth;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,23 +1,84 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_MATCHUP = gql`
-  mutation addUsuer($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
-      _id
-      first_name
-      tech2
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
     }
-  }
-`;
+  }`;
 
-export const CREATE_VOTE = gql`
-  mutation addServicePost($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
+export const ADD_USER = gql`
+  mutation addUser($first_name: String!, $last_name: String!, $username: String!, $email: String!, $password: String!) {
+    addUser(first_name: $first_name, last_name: $last_name, username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id: ID
+        first_name
+        last_name
+        username
+      }
+    }
+  }`;
+
+export const ADD_SERVICEPOST = gql`
+mutation addServicePost($name: String, $description: String, $location:String, $hourly_rate: String, $phone_number: String, $image: String, $user: String) {
+  addServicePost(name: $name, description: $description, location: $location, hourly_rate: $hourly_rate, phone_number: $phone_number, image: $image, user: $user) {
+    user {
+      _id: ID
+      first_name
+      last_name
+      username
+      servicePost {
+        _id
+        name
+        description
+        location
+        hourly_rate
+        phone_number
+      }
     }
   }
-`;
+}`;
+
+export const SAVED_SERVICE_POST = gql `
+mutation savedServicePost($servicePostId: ID!) {
+  savedServicePost(servicePostId: $servicePostId) {
+    _id: ID
+      first_name
+      last_name
+      username
+      servicePost {
+        _id
+        name
+        description
+        location
+        hourly_rate
+        phone_number
+      }
+    
+  }
+}`
+
+export const REMOVE_SERVICE_POST = gql `
+mutation removeServicePost($servicePostId: ID!) {
+  removeServicePost(servicePostId: $servicePostId) {
+    _id: ID
+      first_name
+      last_name
+      username
+      servicePost {
+        _id
+        name
+        description
+        location
+        hourly_rate
+        phone_number
+      }
+    
+  }
+}`
+
+
