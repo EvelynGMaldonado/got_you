98492f9cb1d24185949845797649a27e04d6007f
Jorge Barragan <jmarq19@outlook.com>
2021-12-02 12:20:07 -0800

    adding more front end elements

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./css/Home.css";
 
 import Loginform from '../components/Loginform'
 import MissionBanner from '../components/MissionBanner'
 import ImageBanner from '../components/ImageBanner'
-import IntroSection from '../components/IntroSection'
+// import IntroSection from '../components/IntroSection'
+import Modal from '../components/Modal'
+import ContactusModal from '../components/ContactusModal'
 
 const Home = (props) => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(prev => !prev);
+  };
+
+  const [showContactusModal, setShowContactusModal] = useState(false);
+
+  const openContactusModal = () => {
+    setShowContactusModal(prev => !prev);
+  };
+
+
 
   return (
     <main className="home base-grid home-columns" >
       <MissionBanner/>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <ContactusModal showContactusModal={showContactusModal} setShowContactusModal={setShowContactusModal} />
        <ImageBanner/>
         <Loginform/>
-         <IntroSection/>
+         {/* <IntroSection/> */}
+         <section className="intro">
+        <div className="">
+        <img className="screen" src={require("../assets/screens.jpg").default} style={{maxWidth:"50%"}}alt="pp"/>
+        <div>
+        <h4 className="works">How it works:</h4>
+        <p>IGotcha works by allowing you to connect to someone who is pefect for lending a hand in whatever you need!</p>
+        </div>
+        <div className="full-width distribute-even fit">
+        <button onClick={openModal} className="btnmore ">About us</button>
+        <button onClick={openContactusModal} className="btnmore">Contact us</button>
+        <button className="btnmore">Language</button>
+        </div>
+        </div>
+        </section>
     </main>
 
   );
